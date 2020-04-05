3bc35ae72bd10aeb132b8f34306c0ad2a71083d5
ProvadeRR <stas.karnoza@gmail.com>
2020-04-05 20:34:43 +0300

    Исправлен баг с if на страницах

--- users/pages/client.php
+++ users/pages/client.php
@@ -2,11 +2,9 @@
 session_start();
 
 if (!isset($_SESSION['USER'])) {
-    header("Location: ../../../index.php");
+    header("Location: ../../index.php");
 }
-if ($_SESSION['USER']['role'] == 'Client' || $_SESSION['USER']['role'] == 'Клиент' || $_SESSION['USER']['role'] == 'Клієнт') {
-    exit(header('Location: /error404/'));
-}
+
 include "../../blocks/header.php";
 ?>
 
--- users/pages/manager.php
+++ users/pages/manager.php
@@ -1,8 +1,7 @@
 <?php
 session_start();
-
-if (!isset($_SESSION['USER'])) {
-    header("Location: ../../../index.php");
+if ($_SESSION['USER']['role'] == 'Client' || $_SESSION['USER']['role'] == 'Клиент' || $_SESSION['USER']['role'] == 'Клієнт') {
+    exit(header('Location: /error404/'));
 }
 include "../../blocks/header.php";
 ?>
