99cb2a4c9b007cacdcb3ae7f5491c3806faf212e
ProvadeRR <stas.karnoza@gmail.com>
2020-04-05 20:26:46 +0300

    Исправлено название страниц

--- users/pages/client.php
+++ users/pages/client.php
@@ -4,17 +4,19 @@ session_start();
 if (!isset($_SESSION['USER'])) {
     header("Location: ../../../index.php");
 }
+if ($_SESSION['USER']['role'] == 'Client' || $_SESSION['USER']['role'] == 'Клиент' || $_SESSION['USER']['role'] == 'Клієнт') {
+    exit(header('Location: /error404/'));
+}
 include "../../blocks/header.php";
 ?>
+
 <?php if($_SESSION['USER']['lang'] == "Русский"): ?>
-    <h1> Страница менеджера </h1>
+    <h1> Страница клиента </h1>
 <?php endif;?>
 <?php if($_SESSION['USER']['lang'] == "English"): ?>
-    <h1> Page Manager </h1>
+    <h1> Page Client </h1>
 <?php endif;?>
 <?php if($_SESSION['USER']['lang'] == "Український"): ?>
     <h1> Cторінка Клієнта </h1>
 <?php endif;?>
-
-
 <?php include "../../blocks/footer.php"; ?>
