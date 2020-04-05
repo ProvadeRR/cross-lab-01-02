05c21ac50243fc16497ea2ceb1b9c47ec089a83c
ProvadeRR <stas.karnoza@gmail.com>
2020-04-05 23:37:48 +0300

    Уменьшен файл INDEX.PHP

--- index.php
+++ index.php
@@ -1,19 +1,13 @@
 <?php
 session_start();
 
-if (isset($_SESSION['USER']) && $_SESSION['USER']['lang'] == "Русский") {
-    header("Location: users/profile.php");
+if (!isset($_SESSION['USER'])) {
+    header("Location: /guest/NotAuth/form-auth.php");
 }
-if (isset($_SESSION['USER']) && $_SESSION['USER']['lang'] == "English") {
-    header("Location: users/profile.php");
-}
-if (isset($_SESSION['USER']) && $_SESSION['USER']['lang'] == "Український") {
+if (isset($_SESSION['USER']) && $_SESSION['USER']['lang'] != "") {
     header("Location: users/profile.php");
 }
 if(isset($_SESSION['USER']) && $_SESSION['USER']['lang'] == "")
 {
     header("Location: /guest/isAuth/checkLanguage.php");
-}
-if (!isset($_SESSION['USER'])) {
-    header("Location: /guest/NotAuth/form-auth.php");
 }
